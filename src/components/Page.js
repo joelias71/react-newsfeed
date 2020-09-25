8366534823500b7e42c26c9c2a48f2a05cdf5291
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-09-25 00:06:55 -0500

    feat(handle data) handle data

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,13 +3,21 @@ import Card from '../components/Card'
 import './Page.css'
 import { connect } from 'react-redux'
 
-function Page(props) {
+function Page({data,error,title}) {
     
-    const news = props.state.news.data.map(item => <Card key={item.news_id} item={item} />)
+    let news
+
+    if(error) {
+        news = <h1>Error al cargar los datos.</h1>
+    } else if(data.length === 0) {
+        news = <h1>Loading ...</h1>
+    } else {
+        news = data.map(item => <Card key={item.news_id} item={item} />)
+    }
 
     return (
         <div className='news-container' >
-            <h1 className='news-container__title' >{props.state.news.title}</h1>
+            <h1 className='news-container__title' >{title}</h1>
             <div className='news-container__list' >
                 { news }
             </div>
@@ -19,7 +27,9 @@ function Page(props) {
 
 const mapStateToProps = (state) => {
     return {
-        state
+        data: state.news.data,
+        error: state.news.error,
+        title: state.news.title
     }
 }
 
