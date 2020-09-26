9606622d74dd2ea6fa8f3b33e9c3af0f88ef9ded
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-09-25 21:12:40 -0500

    Updates

---
+++ precache-manifest.6278a9e049e7c687e9e47fcc20f1a3e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08cd3ba5b691ab912b7f7756743e18eb",
+    "url": "/reactjs-newsfeed/index.html"
+  },
+  {
+    "revision": "ae9a1ea0e355e4dd81de",
+    "url": "/reactjs-newsfeed/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ebf04c30efb77892552c",
+    "url": "/reactjs-newsfeed/static/css/main.17dd7f91.chunk.css"
+  },
+  {
+    "revision": "ae9a1ea0e355e4dd81de",
+    "url": "/reactjs-newsfeed/static/js/2.d631a859.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/reactjs-newsfeed/static/js/2.d631a859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf04c30efb77892552c",
+    "url": "/reactjs-newsfeed/static/js/main.6d0cbf76.chunk.js"
+  },
+  {
+    "revision": "82d0715d177444240d43",
+    "url": "/reactjs-newsfeed/static/js/runtime-main.e66c8165.js"
+  }
+]);
